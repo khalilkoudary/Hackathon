caad7f47bc4b9617081107928c825f8d0ad4315c
khalilkoudary <khalilkoudary@gmail.com>
2023-02-04 16:51:56 -0500

    -

--- Calculator.java
+++ Calculator.java
@@ -2,18 +2,31 @@ package Hackathon;
 import java.util.Scanner;
 
 class Calculator{
-    public static void main(String[] argv){
+       public static void main(String[] argv){
+        
+        System.out.println("Welcome to your Student-Budget Calculator!");
+
         Scanner keyIn = new Scanner(System.in);
 
         System.out.println("Enter your income:");
         double income = keyIn.nextDouble();
 
         System.out.println("Enter your rent:");
+        
         double rent = keyIn.nextDouble();
 
         budget student = new budget(income, rent);
 
-        student.getTransportation();
+        student.setBills();
+
+        boolean menu = true;
+
+        while(menu){
+            System.out.println("Please select one of the following options:");
+            System.out.println("1. Transportation\n2. Food \n3. Savings \n4. Personal \n")
+
+
+        }
 
     }
 
