3858cbc54af73d3e7094eb1e166afa42f9a4a160
khalilkoudary <khalilkoudary@gmail.com>
2023-02-04 14:32:04 -0500

    -

---
+++ budget.java
@@ -0,0 +1,14 @@
+package Hackathon;
+
+public class budget {
+    private double rent;
+    private double groceries;
+    private double savings;
+    private double transportation;
+    private double entertainment; 
+    private double bills;
+
+    public budget(double rent){
+        this.rent=rent;
+    }
+}
